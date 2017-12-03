5b83d6571a35bbbf701113ceeb46830b641597fd
BigTsung <yutsungchiu@gmail.com>
2017-12-03 16:45:58 +0800

    Create ground prefab

---
+++ HerbFarm/Assets/Scripts/GroudInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroudInfo  {
+
+	public enum Status
+	{
+		OPEN,
+		CLOSE
+	}
+
+	private Status currentStatus = Status.OPEN;
+
+}
---
+++ HerbFarm/Assets/Scripts/GroundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour {
+
+	public Vector3 originPos;
+	public Vector2 groundResolution;
+	public GameObject groundPrefab;
+
+	private float groundPrefabWidth;
+	private float groundPrefabHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		groundPrefabWidth = groundPrefab.transform.GetChild (0).GetComponent<Renderer> ().bounds.size.x;
+		groundPrefabHeight = groundPrefab.transform.GetChild (0).GetComponent<Renderer> ().bounds.size.y;
+
+		for (int x = 0; x < groundResolution.x; x++) 
+		{
+			for(int z = 0; z < groundResolution.y; z++)
+			{
+				Vector3 pos = new Vector3();
+				pos.x = originPos.x + groundPrefabWidth * x;
+				pos.z =  originPos.z + groundPrefabWidth * z;
+				Instantiate (groundPrefab, pos, Quaternion.identity);
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
