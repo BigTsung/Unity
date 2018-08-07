619cfd857d2a2315843f7ade7147c8a5f255863c
BigTsung <yutsungchiu@gmail.com>
2018-08-07 23:57:54 +0800

    update player controller - switch animation

--- ARZombie/Assets/PlayerController.cs
+++ ARZombie/Assets/PlayerController.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public static class AnimationState
+    {
+        public static string IDLE { get { return "Idle"; } }
+        public static string RUN { get { return "Run"; } }
+        public static string SHOOT { get { return "Shoot"; } }
+        public static string DEPTH { get { return "Depth"; } }
+    }
 
     // public
     public float m_Speed = 12f;
     public float m_turnSpeed = 180f;
+    public float transitionDuration = 0.1f;
 
     // private
+    private Animator m_animator;
     private float m_movementInputValue;
     private float m_turnInputValue;
     private Rigidbody m_rigidbody;
     private Quaternion targetRotation;
-
 
     private void Awake()
     {
@@ -26,57 +34,60 @@ public class PlayerController : MonoBehaviour {
     {
         targetRotation = transform.rotation;
         m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
         m_movementInputValue = Input.GetAxis("Vertical");
         m_turnInputValue = Input.GetAxis("Horizontal");
 
         //Debug.Log(m_movementInputValue + " " + m_turnInputValue);
+
+        // === Switch animation ===
+        // Move
+        if (m_movementInputValue <= 0.15f)
+        {
+            if (!m_animator.GetCurrentAnimatorStateInfo(0).IsName(AnimationState.IDLE) && !m_animator.IsInTransition(0))
+            {
+                m_animator.CrossFadeInFixedTime(AnimationState.IDLE, transitionDuration);
+            }
+        }
+        else
+        {
+            if (!m_animator.GetCurrentAnimatorStateInfo(0).IsName(AnimationState.RUN) && !m_animator.IsInTransition(0))
+            {
+                m_animator.CrossFadeInFixedTime(AnimationState.RUN, transitionDuration);
+            }
+        }
+
+        // Shoot
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Shooting!!!");
+            //if (!m_animator.IsInTransition(0))
+            m_animator.CrossFadeInFixedTime(AnimationState.SHOOT, transitionDuration);
+        }
+
+        //Debug.Log("Is Idle? " + m_animator.GetCurrentAnimatorStateInfo(0).IsName(AnimationState.IDLE));
+        //Debug.Log("Is Run? " + m_animator.GetCurrentAnimatorStateInfo(0).IsName(AnimationState.RUN));
     }
 
     private void FixedUpdate()
     {
-        // Adjust the rigidbodies position and orientation in FixedUpdate.
         Move();
         Turn();
     }
 
     private void Move()
     {
-        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-        //Vector3 movement = transform.forward * m_movementInputValue * m_Speed * Time.deltaTime;
-
-        // Apply this movement to the rigidbody's position.
-        //m_rigidbody.MovePosition(m_rigidbody.position + movement);
-
-        //transform.Rotate(0, x, 0);
-        //transform.Translate(Vector3.forward * input * moveSpeed);
-        //transform.Translate(movement);
-
         m_rigidbody.velocity = transform.forward * m_movementInputValue * m_Speed;
-
-        // Debug.Log(transform.forward * m_movementInputValue * m_Speed);
     }
 
 
     private void Turn()
     {
-        // Determine the number of degrees to be turned based on the input, speed and time between frames.
-        //float turn = m_turnInputValue * m_turnSpeed * Time.deltaTime;
-
-        // Make this into a rotation in the y axis.
-        //Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-
-        // Apply this rotation to the rigidbody's rotation.
-        // m_rigidbody.MoveRotation(m_rigidbody.rotation * turnRotation);
-        // transform.Rotate(0, turn, 0);
-        //transform.Translate(0, 0, turn);
-
         targetRotation *= Quaternion.AngleAxis(m_turnInputValue * m_turnSpeed * Time.deltaTime, Vector3.up) ;
-
         transform.rotation = targetRotation;
     }
 }
