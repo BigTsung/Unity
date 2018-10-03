1425c68b9cb4cbb81fe7fed5213ac19f62d6e005
BigTsung <yutsungchiu@gmail.com>
2018-10-03 11:27:19 +0800

    Added a new behaviour when running

--- ARZombie/Assets/Scripts/Gameplay/TargetScanner.cs
+++ ARZombie/Assets/Scripts/Gameplay/TargetScanner.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 public class TargetScanner
 {
     //public float heightOffset = 0.0f;
-    public float detectionRadius = 10;
+    public float detectionRadius = 10f;
+    public float followRadius = 7f;
     //[Range(0.0f, 360.0f)]
     //public float detectionAngle = 270;
     //public float maxHeightDifference = 1.0f;
@@ -24,14 +25,16 @@ public class TargetScanner
 
     public void EditorGizmo(Transform transform)
     {
-        Color c = new Color(0, 0, 0.7f, 0.4f);
+        Color c_detect = new Color(0, 0, 0.7f, 0.4f);
+        Color c_Follow = new Color(0.7f, 0, 0f, 0.4f);
 
-        UnityEditor.Handles.color = c;
+        UnityEditor.Handles.color = c_detect;
         Vector3 rotatedForward = Quaternion.Euler(0, -360f * 0.5f, 0) * transform.forward;
         UnityEditor.Handles.DrawSolidArc(transform.position, Vector3.up, rotatedForward, 360f, detectionRadius);
 
-        //Gizmos.color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
-        //Gizmos.DrawWireSphere(transform.position + Vector3.up * heightOffset, 0.2f);
+        UnityEditor.Handles.color = c_Follow;
+        //Vector3 rotatedForward = Quaternion.Euler(0, -360f * 0.5f, 0) * transform.forward;
+        UnityEditor.Handles.DrawSolidArc(transform.position, Vector3.up, rotatedForward, 360f, followRadius);
     }
 
 #endif
--- ARZombie/Assets/Scripts/Gameplay/ZombieBehaviour.cs
+++ ARZombie/Assets/Scripts/Gameplay/ZombieBehaviour.cs
@@ -8,12 +8,29 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Collider))]
 public class ZombieBehaviour : MonoBehaviour {
 
+    private static string Ani_Walk      = "Walk";
+    private static string Ani_Run       = "Run";
+    private static string Ani_Damage    = "Damage";
+    private static string Ani_Idle      = "Idle";
+    private static string Ani_Attack    = "Attack";
+
+
     public TargetScanner targetScanner;
 
     private NavMeshAgent agent;
     private Animator animator;
     private Collider collider;
-    
+    private bool fighting = false;
+    private Vector3 spawnPosition;
+
+    public enum StatusWithTarget
+    {
+        NONE,
+        TOOFAR,
+        ARRIVED_TARGET,
+        KEEP_GOING
+    }
+
     public Transform Target
     {
         get;
@@ -22,10 +39,34 @@ public class ZombieBehaviour : MonoBehaviour {
     void Start ()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
 
         SceneLinkedSMB<ZombieBehaviour>.Initialise(animator, this);
 
         Debug.Log("ZombieBehaviour Start");
+
+    }
+
+    private void OnEnable()
+    {
+        spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (agent.isStopped)
+            return;
+
+        //Debug.Log("GOGOGO");
+
+        if (fighting && Target != null)
+        {
+            agent.SetDestination(Target.position);
+        }
+        else
+        {
+            agent.SetDestination(spawnPosition);
+        }
     }
 
     private void SetAnimatorTrigger(string triggerName)
@@ -36,15 +77,18 @@ public class ZombieBehaviour : MonoBehaviour {
         }
     }
 
+    // ===========================================
+    // Function for Behaviour
+    // ===========================================
     public Transform Detect()
     {
         if (Playermanager.Instance.ExistPlayer)
         {
-            Debug.Log("Exist the player");
+            //Debug.Log("Exist the player");
             List<Transform> playlist = Playermanager.Instance.PlayerList;
             for (int i = 0; i < playlist.Count; i++)
             {
-                Debug.Log(Vector3.Distance(playlist[i].position, transform.position) <= targetScanner.detectionRadius);
+                //Debug.Log(Vector3.Distance(playlist[i].position, transform.position) <= targetScanner.detectionRadius);
                 if (Vector3.Distance(playlist[i].position, transform.position) <= targetScanner.detectionRadius)
                 {
                     Target = playlist[i];
@@ -57,10 +101,55 @@ public class ZombieBehaviour : MonoBehaviour {
 
     public void GotoTarget()
     {
-        SetAnimatorTrigger("Walk");
-
+        SetAnimatorTrigger(Ani_Run);
+        agent.enabled = true;
+        fighting = true;
     }
 
+    public StatusWithTarget GetStatusWithTarget()
+    {
+        StatusWithTarget status = StatusWithTarget.NONE;
+        float dis = -1f;
+        if (Target != null)
+        {
+            dis = Vector3.Distance(Target.position, this.transform.position);
+
+            Debug.Log(dis);
+            if (dis >= targetScanner.followRadius)
+            {
+                status = StatusWithTarget.TOOFAR;
+            }
+            else if(dis <= agent.stoppingDistance + 1.2f)
+            {
+                status = StatusWithTarget.ARRIVED_TARGET;
+            }
+            else
+            {
+                status = StatusWithTarget.KEEP_GOING;
+            }
+        }
+        else
+        {
+            status = StatusWithTarget.NONE;
+        }
+
+        return status;
+    }
+
+    public void BackToSpawnPosiion()
+    {
+        SetAnimatorTrigger(Ani_Walk);
+        fighting = false;
+    }
+
+    public void Fighting()
+    {
+        SetAnimatorTrigger(Ani_Attack);
+    }
+
+    // ===========================================
+    // Function for Drawing
+    // ===========================================
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
