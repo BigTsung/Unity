6148f2967639e72101957595a1007a177e040aba
BigTsung <yutsungchiu@gmail.com>
2018-11-27 23:20:23 +0800

    Added new simple main menu scene

---
+++ ARZombie/Assets/MainMenuUIManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MainMenuUIManager : MonoBehaviour {
+
+    public void Go()
+    {
+        SceneSwitcher.Instance.LoadScene("Main");
+    }
+}
---
+++ ARZombie/Assets/Scripts/Gameplay/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : Singleton<SceneSwitcher> {
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneAsync(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
