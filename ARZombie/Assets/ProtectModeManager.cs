f558f56297d335ac3ac692184bb0243f7c8ec263
Anson Chiu <anson@aemass.com>
2019-02-08 22:45:02 +0800

    upgrade ProtectModeManager

--- ARZombie/Assets/ProtectModeManager.cs
+++ ARZombie/Assets/ProtectModeManager.cs
@@ -15,37 +15,77 @@ public class ProtectModeManager : Singleton<ProtectModeManager> {
     public class EnemyInfo
     {
         public ObjectPooler.ObjectTag enemyTag;
+        public Transform spawnTrans;
         public int num;
+    }
+
+    public int CurrentLevel
+    {
+        get;
+        set;
     }
 
     public float startWaitingTime = 20f;
     public List<LevelInfo> levelSetting = new List<LevelInfo>();
 
     private float countDownRefreshTime = 0.1f;
-    private int currentLevel = 1;
+    private LevelInfo currentLevelInfo = new LevelInfo();
+    private int needSpawnedEnemyNum = 0;
+    //private int currentLevel = 0;
+
+    private void Start()
+    {
+        CurrentLevel = 0;
+        StartAll();
+    }
+
+    public void StartAll()
+    {
+        InvokeRepeating("Countdown", 0, countDownRefreshTime);
+    }
 
     private void Countdown()
     {
         startWaitingTime -= countDownRefreshTime;
 
+        Debug.Log(startWaitingTime);
+
         if (startWaitingTime <= 0)
         {
             CancelInvoke("Countdown");
-            SpawnEnemy();
+            StartLevel();
             return;
         }
 
         // Refresh everything!!!
     }
 
-    private void SpawnEnemy()
+    private void StartLevel()
     {
-         
+        if (CurrentLevel < levelSetting.Count)
+        {
+            currentLevelInfo = levelSetting[CurrentLevel];
+            needSpawnedEnemyNum = 0;
+
+            for (int i = 0; i < currentLevelInfo.enemyList.Count; i++)
+            {
+                needSpawnedEnemyNum += currentLevelInfo.enemyList[i].num;
+            }
+
+            InvokeRepeating("SpawnEnemy", 0, currentLevelInfo.spacingTime);
+        }
+        else
+        { }
     }
 
-    public void StartAll()
+   private void SpawnEnemy()
     {
-        InvokeRepeating("Countdown", 0, countDownRefreshTime);
+        //ObjectPooler.Instance.SpawnFormPool(;
+
+        needSpawnedEnemyNum--;
+
+        if (needSpawnedEnemyNum <= 0)
+            CancelInvoke("SpawnEnemy");
     }
 
 
