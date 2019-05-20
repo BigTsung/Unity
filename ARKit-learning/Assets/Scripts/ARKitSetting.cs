72275f8b311add6dd63444fe9102c05fb86cfc3a
CHiu <chiu@CHius-MacBook-Pro.local>
2019-05-20 17:44:03 +0800

    Upload ARKitSwitch. That can support switch ARKit

---
+++ ARKit-learning/Assets/Scripts/ARKitSetting.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class ARKitSetting : MonoBehaviour
+{
+    [System.Serializable]
+    public enum ConfigurationType
+    {
+        Basic,
+        FaceTracking,
+        WorldTracking,
+        ObjectScanning
+    }
+
+    [SerializeField] private ConfigurationType defaultConfiguration;
+
+    private UnityARSessionNativeInterface m_session;
+
+    private bool m_currentARkitStatus = false;
+
+    private void Start()
+    {
+        m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+
+        RunSpecificConfiguration(defaultConfiguration);
+    }
+
+    public bool ARkitStats
+    {
+        get {
+            return m_currentARkitStatus;
+        }
+    }
+
+    public void RunSpecificConfigurationByIndex(int ind)
+    {
+        RunSpecificConfiguration((ConfigurationType)ind);
+    }
+
+    public void RunSpecificConfiguration(ConfigurationType type)
+    { 
+        switch(type)
+        {
+            case ConfigurationType.Basic:
+                ARKitSessionConfiguration basicConfig = new ARKitSessionConfiguration();
+                RunARkit(basicConfig);
+                break;
+            case ConfigurationType.FaceTracking:
+                ARKitFaceTrackingConfiguration faceTrackingConfig = new ARKitFaceTrackingConfiguration();
+                RunARkit(faceTrackingConfig);
+                break;
+            case ConfigurationType.ObjectScanning:
+                ARKitObjectScanningSessionConfiguration objectScanningConfig = new ARKitObjectScanningSessionConfiguration();
+                RunARkit(objectScanningConfig);
+                break;
+            case ConfigurationType.WorldTracking:
+                ARKitWorldTrackingSessionConfiguration worldTrackingConfig = new ARKitWorldTrackingSessionConfiguration();
+                RunARkit(worldTrackingConfig);
+                break;
+            default:
+                Debug.Log("your config type is wrong!");
+                break;
+        }
+    }
+
+    public void PauseARkit()
+    {
+        if (m_currentARkitStatus == true)
+        {
+            m_session.Pause();
+            m_currentARkitStatus = false;
+        }
+    }
+
+    private void RunARkit(ARKitSessionConfiguration configuration)
+    {
+        if (configuration.IsSupported && m_currentARkitStatus == false)
+        {
+            m_session.RunWithConfig(configuration);
+            m_currentARkitStatus = true;
+        }
+    }
+
+    private void RunARkit(ARKitFaceTrackingConfiguration configuration)
+    {
+        if (configuration.IsSupported && m_currentARkitStatus == false)
+        {
+            m_session.RunWithConfig(configuration);
+            m_currentARkitStatus = true;
+        }
+    }
+
+    private void RunARkit(ARKitWorldTrackingSessionConfiguration configuration)
+    {
+        if (configuration.IsSupported && m_currentARkitStatus == false)
+        {
+            m_session.RunWithConfig(configuration);
+            m_currentARkitStatus = true;
+        }
+    }
+
+    private void RunARkit(ARKitObjectScanningSessionConfiguration configuration)
+    {
+        if (configuration.IsSupported && m_currentARkitStatus == false)
+        {
+            m_session.RunWithConfig(configuration);
+            m_currentARkitStatus = true;
+        }
+    }
+}
